d845ef4f53b853500220323255f756885f5e8089
davisfink <me@davisfink.com>
2015-04-03 14:03:42 -0400

    Added monster and character objects

--- EyeTyrant-App/lib/EyeTyrant/App.pm
+++ EyeTyrant-App/lib/EyeTyrant/App.pm
@@ -1,12 +1,34 @@
 package EyeTyrant::App;
+
+use strict;
+use warnings;
+
 use Dancer2;
+use Data::Dumper;
+use Dancer2::Template::TemplateToolkit;
+use lib "..";
+use EyeTyrant::DataObjects::Character::Manager;
+use EyeTyrant::DataObjects::Monster::Manager;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
+    my $monsters   = EyeTyrant::DataObjects::Monster::Manager->get_objects;
+    my $characters = EyeTyrant::DataObjects::Character::Manager->get_objects;
+    my $character_array = [
+        map {
+                {
+                    name => $_->name,
+                    race => $_->race,
+                }
+            } @$characters
+    ];
+
     template 'index', {
-        'msg' => "Test message",
+        'msg'        => "Test message",
         'page_title' => "eye tyrant",
+        'people'     => $characters,
+        'monsters'   => $monsters,
     };
 };
 
