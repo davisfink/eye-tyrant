08cadb2f03518c39c5be3d97cb5205f212e89422
davisfink <me@davisfink.com>
2016-11-17 20:35:47 -0500

    things about things

--- main.rb
+++ main.rb
@@ -7,6 +7,7 @@ get '/' do
     template = Tilt.new('templates/index.erb')
     DB = Sequel.connect('mysql://root:Kadena.1@localhost:3306/eye_tyrant')
     monsters = DB[:monster]
+    dragons = monsters.where(Sequel.like(:name, "%dragon%"))
 
-    template.render(self, :title => monsters.first[:name])
+    template.render(self, :title => dragons.first[:name])
 end
