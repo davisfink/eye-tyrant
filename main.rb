0e7d27749424fbbc6117bc58bfdd762bb48dba32
Davis Fink <davisfink@Daviss-MacBook-Pro.local>
2017-12-21 16:11:58 -0500

    updated more things for ruuuubys

--- main.rb
+++ main.rb
@@ -4,12 +4,13 @@ require './database.rb'
 require './models/encounter.rb'
 require './models/monster.rb'
 require './models/encounter_participant.rb'
+require './models/experience.rb'
 #require './models/spell.rb'
 #require './models/action.rb'
 
 get '/' do
     @current_encounter = Encounter.where(:active, "1").first
-    @participants = EncounterParticipant.where(:encounter_id, @current_encounter.id.to_s)
+    @participants = EncounterParticipant.where(:encounter_id, @current_encounter.id.to_s).order(Sequel.desc(:initiative))
 
     erb :index
 end
@@ -21,11 +22,12 @@ get '/find-monster/:search_term/*' do
     erb :index
 end
 
-get '/set-initiative/:id/:value/*' do
-    puts 'taaacos'
-    puts params[:id]
-    puts params[:value]
-    EncounterParticipant.find(:id, params[:id]).update(initiative: params[:value])
+get '/set-initiative/' do
+    EncounterParticipant.find(id: params[:id]).update(initiative: params[:value])
+    redirect '/'
+end
 
-    erb :index
+get '/set-damage/' do
+    EncounterParticipant.find(id: params[:id]).update(damage: params[:value])
+    redirect '/'
 end
---
+++ models/experience.rb
@@ -0,0 +1,3 @@
+class Experience < Sequel::Model(:experience)
+    dataset
+end
