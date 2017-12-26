36255072b41208827a107116824dda5daaa51169
Davis Fink <davis.fink@gmail.com>
2017-12-26 14:58:19 -0500

    updated participant rb

--- models/participant.rb
+++ models/participant.rb
@@ -1,18 +1,9 @@
+require './modules/participant_actions.rb'
 class Participant < Sequel::Model(:participant)
+    include ParticipantActions
+
     def initialize(id)
         Participant.find_or_create(id)
-    end
-
-    def take_damage(value)
-        @damage += value ? @damage += value < @hitpoints : @damage = @hitpoints
-    end
-
-    def heal_damage(value)
-        @damage -= value ? @damage == value > 0 : @damage = 0
-    end
-
-    def set_initiative(value)
-        @initiative = value
     end
 
 end
